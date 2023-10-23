a3cd3a43138ea9fbdf4c325ba9471844ba1509d3
carlosandf <carlos.andres.ff2912@gmail.com>
2023-10-23 10:55:06 -0500

    Fix: add overflow-wrap to text

--- list-items/src/components/item.tsx
+++ list-items/src/components/item.tsx
@@ -2,7 +2,7 @@ import { DeleteIcon } from './icons';
 
 export const Item = ({ text, handleClick }: {text: string, handleClick: () => void}) => {
   return (
-    <li className='flex justify-between gap-2 bg-slate-700 px-1.5 py-1 rounded'>
+    <li style={{ 'overflow-wrap': 'anywhere' }} className='flex justify-between gap-2 bg-slate-700 px-1.5 py-1 rounded break-before-column'>
       <p>{text}</p>
       <button onClick={handleClick} className='cursor-pointer rounded w-fit h-fit hover:bg-slate-500 transition-colors'>
         <DeleteIcon />
